9765645ed1ad999f1174f6f78f1d2d088e9f6b33
Guillaume GENCE <guillaume.gence@gmail.com>
2017-09-05 13:08:11 +0200

    add base du langage

---
+++ scripts/bases/test.js
@@ -0,0 +1,54 @@
+var a;
+a = "bob";
+a += a;
+var n = 12;
+n = 0.13;
+var v;
+console.log(v)
+
+
+function direBonjour() {
+  console.log("Bonjour");
+}
+
+
+function Eleve(prenom,nom) {
+    this.prenom = prenom;
+    this.nom = nom
+    this.salutation = function () {
+      console.log("Bonjour je suis "+ this.prenom + " " + this.nom);
+    }
+}
+
+var eleve1 = new Eleve("Bob","DYLAN");
+eleve1.salutation();
+
+var ajd = new Date();
+console.log(ajd);
+
+"5" == 5
+"5" === 5
+
+if (true || false) {
+
+} else {
+
+}
+
+var expression = "a";
+switch (expression) {
+  case expression === "a":
+  case expression === "b":
+
+    break;
+  case expression === "c":
+  default:
+
+}
+
+var array = []
+for (var i = 0; i < 10; i++) {
+  array[i] = Math.random();
+}
+
+console.log(array);
