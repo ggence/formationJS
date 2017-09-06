6a7d0485b4fbc2cf9aadfd2a04e8a9e05253d224
Guillaume GENCE <guillaume.gence@gmail.com>
2017-09-06 13:42:04 +0200

    Ajout des callbacks

--- scripts/bases/test.js
+++ scripts/bases/test.js
@@ -11,23 +11,9 @@ function direBonjour() {
     console.log("Bonjour");
 }
 
-
-function Eleve(prenom, nom) {
-    this.prenom = prenom;
-    this.nom = nom
-    this.salutation = function() {
-        console.log("Bonjour je suis " + this.prenom + " " + this.nom);
-    }
-}
-
-var eleve1 = new Eleve("Bob", "DYLAN");
-eleve1.salutation();
-
-var ajd = new Date();
-console.log(ajd);
-
-"5" == 5
-    "5" === 5
+var test1 = "5" == 5;
+var test2 = "5" === 5;
+var test3 = test1 || test2;
 
 if (true || false) {
 
@@ -45,6 +31,25 @@ switch (expression) {
   default:
 
 }
+
+
+setTimeout( direBonjour, 10000);
+
+setInterval( function () { console.log("Je reviens dans 3 secondes") }, 3000);
+
+function Eleve(prenom, nom) {
+    this.prenom = prenom;
+    this.nom = nom
+    this.salutation = function() {
+        console.log("Bonjour je suis " + this.prenom + " " + this.nom);
+    }
+}
+
+var eleve1 = new Eleve("Bob", "DYLAN");
+eleve1.salutation();
+
+var ajd = new Date();
+console.log(ajd);
 
 var array = []
 for (var i = 0; i < 10; i++) {
